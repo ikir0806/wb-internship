044a6e0ff4c23f13c470b1896b997345ab7d1996
ikir0806 <ikir2826@gmail.com>
2022-11-26 23:12:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,252 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var eventCalllback = function (e) {
+        var el = e.target,
+            clearVal = el.dataset.phoneClear,
+            pattern = el.dataset.phonePattern,
+            matrix_def = "+7(___) ___-__-__",
+            matrix = pattern ? pattern : matrix_def,
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = e.target.value.replace(/\D/g, "");
+        if (clearVal !== 'false' && e.type === 'blur') {
+            if (val.length < matrix.match(/([\_\d])/g).length) {
+                e.target.value = '';
+                return;
+            }
+        }
+        if (def.length >= val.length) val = def;
+        e.target.value = matrix.replace(/./g, function (a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+    }
+    var phoneMask = document.querySelector('.receiver__data-number');
+    for (let ev of ['input', 'focus']) {
+        phoneMask.addEventListener(ev, eventCalllback);
+    }
+});
+
+function rotateArrow(element) {
+    let rotationEl = element.querySelector('.icon--arrow')
+    let visibility = element.closest('div')
+        .nextElementSibling.style
+
+    if (!rotationEl.classList.contains('icon--arrow-rotated'))
+        visibility.display = 'none'
+    else visibility.display = 'flex'
+    rotationEl.classList.toggle('icon--arrow-rotated')
+}
+
+function likeItem(element) {
+    let heartColor = element.querySelector('svg')
+    heartColor.classList.toggle('icon--heart-selected')
+}
+
+function validate() {
+    let inputs = document.querySelectorAll('.receiver__input')
+
+    document.querySelector('.receiver__data-subtext')
+        .style.display = 'block'
+
+    let submit = false
+
+    let inputFirstName = document
+        .querySelector('.receiver__first-name')
+    let inputSecondName = document
+        .querySelector('.receiver__second-name')
+    let inputEmail = document
+        .querySelector('.receiver__data-email')
+    let inputPhone = document
+        .querySelector('.receiver__data-number')
+    let inputInn = document
+        .querySelector('.receiver__data-inn')
+
+    nameValidate(inputFirstName)
+    nameValidate(inputSecondName)
+    emailValidate(inputEmail)
+    phoneValidate(inputPhone)
+    innValidate(inputInn)
+
+    if (submit) {
+        console.log('Данные ушли')
+        inputs.forEach((input) => {
+            input.value = ''
+        })
+    }
+}
+
+function nameValidate(name) {
+    if (name.value === '') {
+        name.classList.add('receiver__error')
+        name.nextElementSibling.style.display = 'block'
+        return true
+    }
+    else blurInputs(name)
+}
+
+function emailValidate(email) {
+    if (!email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        email.classList.add('receiver__error')
+        email.nextElementSibling.style.display = 'block'
+        return true
+    }
+    else blurInputs(email)
+}
+
+function phoneValidate(phone) {
+    if (phone.value.length !== 17) {
+        phone.classList.add('receiver__error')
+        phone.nextElementSibling.style.display = 'block'
+        return true
+    }
+    else blurInputs(phone)
+}
+
+function innValidate(inn) {
+    if ((inn.value.length >= 10) || (!inn.value.match(/^\d+$/))) {
+        inn.classList.add('receiver__error')
+        inn.nextElementSibling.style.display = 'block'
+        document.querySelector('.receiver__data-subtext')
+            .style.display = 'none'
+        return true
+    }
+    else blurInputs(inn)
+}
+
+function blurInputs(element) {
+    element.classList.remove('receiver__error')
+    element.nextElementSibling.style.display = 'none'
+}
+
+function showManufacturer(element) {
+    element.nextElementSibling.style.display = 'block'
+}
+
+function hideManufacturer(element) {
+    element.nextElementSibling.style.display = 'none'
+}
+
+function showPrice(element) {
+    element.nextElementSibling.style.display = 'flex'
+}
+
+function hidePrice(element) {
+    element.nextElementSibling.style.display = 'none'
+}
+
+function showInfo(element) {
+    element.closest('p')
+        .nextElementSibling.style.display = 'block'
+
+    let coords = element.getBoundingClientRect();
+    let infoEl = document
+        .querySelector('.delivery__refusal-info')
+    infoEl.style.left = coords.left - 115 + "px";
+}
+
+function hideInfo(element) {
+    element.closest('p')
+        .nextElementSibling.style.display = 'none'
+}
+
+function setActive(element) {
+    if (!element.classList
+        .contains('delivery-choice__active')) {
+        let elements = document
+            .querySelectorAll('.delivery-choice__button')
+        elements.forEach((button) => {
+            button.classList.toggle('delivery-choice__active')
+        })
+        elements = document
+            .querySelectorAll('.delivery-choice__adresses')
+        elements.forEach((list) => {
+            list.classList
+                .toggle('delivery-choice__adresses-active')
+        })
+    }
+}
+
+function chooseAdress(element) {
+    let adress = document
+        .querySelector('.radio:checked').closest('label')
+        .nextElementSibling.textContent
+    document.querySelector('.summary__delivery-text')
+        .textContent = adress
+    document.querySelector('.delivery__point-adress')
+        .textContent = adress
+
+    activeChoice = document
+        .querySelector('.delivery-choice__active').textContent
+
+    if (activeChoice === 'Курьером') {
+        document.querySelector('.delivery__point-info')
+            .style.display = 'none'
+        document.querySelector('.summary__delivery-title')
+            .textContent = 'Доставка курьером'
+        document.querySelector('.delivery__point-question')
+            .textContent = 'Курьером'
+    }
+    else {
+        document.querySelector('.delivery__point-info')
+            .style.display = 'flex'
+        let rating = document.querySelector('.radio:checked')
+            .closest('li')
+            .querySelector('.delivery-choice__point-rating')
+            .textContent
+        let time = document.querySelector('.delivery-choice__point-time').textContent
+        document.querySelector('.summary__delivery-title')
+            .textContent = 'Доставка в пункт выдачи'
+        document.querySelector('.delivery__point-question')
+            .textContent = 'Пункт выдачи'
+        document.querySelector('.delivery__point-rating')
+            .textContent = rating
+        document.querySelector('.delivery__point-time')
+            .textContent = time
+    }
+    element.closest('.choice__shadow').style.display = 'none'
+}
+
+function closeChoice(element) {
+    element.closest('.choice__shadow').style.display = 'none'
+}
+
+function openDeliveryChoice() {
+    document.querySelector('.delivery-choice__shadow')
+        .style.display = 'block'
+}
+
+function openPaymentChoice() {
+    document.querySelector('.payment-choice__shadow')
+        .style.display = 'block'
+}
+
+function chooseCard(element) {
+    let cardImg = document.querySelector('.radio:checked')
+        .closest('label').nextElementSibling
+        .querySelector('img').getAttribute('src')
+    let cardNumber = document.querySelector('.radio:checked')
+        .closest('label').nextElementSibling
+        .querySelector('p').textContent
+    let cardSpan = document.querySelector('.radio:checked')
+        .closest('label').nextElementSibling
+        .querySelector('span').textContent
+
+    document.querySelectorAll('.set-card-img')
+        .forEach(el => { el.src = cardImg })
+    document.querySelectorAll('.set-card-num')
+        .forEach(el => { el.textContent = cardNumber })
+    document.querySelector('.set-card-span')
+        .textContent = cardSpan
+    element.closest('.choice__shadow')
+        .style.display = 'none'
+}
+
+function changeButton() {
+    document.querySelector('.summary__button')
+        .classList.toggle('summary__button-white')
+}
+
+function buttonDisabled() {
+    const buttonEl = document.querySelector('.summary__button')
+    if (buttonEl.disabled) buttonEl.disabled = false
+    else buttonEl.disabled = true
+}
---
+++ counter.js
@@ -0,0 +1,483 @@
+let items = [
+    {
+        id: 0,
+        price: 522,
+        totalPrice: 522,
+        pastPrice: 1022,
+        pastTotalPrice: 1022,
+        amount: 1,
+        totalAmount: 3,
+        img: 'item-1',
+    },
+    {
+        id: 1,
+        price: 10500.24,
+        totalPrice: 2100047,
+        pastPrice: 11500.24,
+        pastTotalPrice: 2300047,
+        amount: 200,
+        totalAmount: 250,
+        img: 'item-2',
+    },
+    {
+        id: 2,
+        price: 247,
+        totalPrice: 494,
+        pastPrice: 475,
+        pastTotalPrice: 950,
+        amount: 2,
+        totalAmount: 4,
+        img: 'item-3',
+    },
+]
+
+function round(num) {
+    return +num.toFixed(2)
+}
+
+const deliveryDiv = document.querySelector('.delivery__date-answer')
+let deliveryRow = null
+let itemsPerRow = null
+
+const itemListEl = document.querySelector('.main__list')
+let summaryPrice = 0
+let summaryPastPrice = 0
+let cartAmount = document.querySelector('.header__cart')
+    .querySelector('span')
+cartAmount.textContent = `${items.length}`
+
+items.forEach((item) => {
+    summaryPrice += item.totalPrice
+    summaryPastPrice += item.pastTotalPrice
+
+    let element = document
+        .querySelector('.cart[data-id="' + item.id + '"]')
+    element.querySelector('.cart__amount-num')
+        .textContent = item.amount
+    element.querySelector('.cart__price-current')
+        .textContent = `${(item.totalPrice).toLocaleString('ru-RU')} сом`
+    element.querySelector('.cart__price-past')
+        .textContent = `${(item.pastTotalPrice)
+            .toLocaleString('ru-RU')} сом`
+
+    let amountLeft = item.totalAmount - item.amount
+    if (amountLeft < 3) element
+        .querySelector('.cart__amount-availability')
+        .textContent = `Осталось ${amountLeft} шт.`
+
+    itemsPerRow = item.amount
+    if (item.amount > 184 &&
+        !document
+            .querySelector('.delivery__date')
+            .classList.contains('delivery__date-additional')) {
+        addDeliveryDate(item.id, item.amount - 184, item.img)
+        itemsPerRow = 184
+    }
+
+    deliveryRow = `
+    <div>
+        <img data-id="${item.id}" class="delivery__img" src="./images/${item.img}.jpg" width="40" height="56"
+        alt="${item.img}">
+        <span>${itemsPerRow}</span>
+    </div>
+    `
+    document.querySelector('.delivery__date-answer')
+        .insertAdjacentHTML('beforeend', deliveryRow);
+})
+
+let summaryPriceText = document
+    .querySelector('.summary__result')
+summaryPriceText.textContent = `${(summaryPrice)
+    .toLocaleString('ru-RU')} сом`
+let summaryPastPriceText = document
+    .querySelector('.summary__goods-result')
+summaryPastPriceText.textContent = `${(summaryPastPrice)
+    .toLocaleString('ru-RU')} сом`
+let summaryCost = document
+    .querySelector('.summary__sale-result')
+updateCost()
+
+
+itemListEl.addEventListener('click', e => {
+    if (!e.target.classList.contains('cart__amount-minus')
+        && !e.target.classList.contains('cart__amount-plus'))
+        return
+
+    const closestItem = e.target.closest('.cart')
+    const id = +closestItem.dataset.id
+    const price = items[id].price
+    const pastPrice = items[id].pastPrice
+
+    let newPriceEl = document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__price-current')
+    let newAmountEl = e.target.closest('div')
+        .querySelector('.cart__amount-num')
+    let newPastPriceEl = document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__price-past')
+
+    e.target.classList.contains('cart__amount-minus')
+        ? itemMinus(id, price, newPriceEl, newAmountEl)
+        : itemPlus(id, price, newPriceEl, newAmountEl)
+
+    e.target.classList.contains('cart__amount-minus')
+        ? itemPastMinus(id, pastPrice, newPastPriceEl)
+        : itemPastPlus(id, pastPrice, newPastPriceEl)
+})
+
+function itemPastPlus(id, pastPrice, newPastPriceEl) {
+    if (document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-plus')
+        .disabled) return
+
+    newPastPriceEl.textContent =
+        `${(round(items[id].pastTotalPrice + pastPrice))
+            .toLocaleString('ru-RU')} сом`
+    items[id].pastTotalPrice = round(items[id]
+        .pastTotalPrice + pastPrice)
+    increaseSummaryPastPrice(pastPrice)
+    updateCost()
+}
+
+function itemPastMinus(id, pastPrice, newPastPriceEl) {
+    if (document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-minus')
+        .disabled) return
+
+    newPastPriceEl.textContent =
+        `${(round(items[id].pastTotalPrice - pastPrice))
+            .toLocaleString('ru-RU')} сом`
+    items[id].pastTotalPrice = round(items[id]
+        .pastTotalPrice - pastPrice)
+    reduceSummaryPastPrice(pastPrice)
+    updateCost()
+}
+
+function itemPlus(id, price, newPriceEl, newAmountEl) {
+    if (items[id].amount == 0) {
+        document
+            .querySelector('.cart[data-id="' + id + '"]')
+            .querySelector('.cart__amount-button-minus')
+            .disabled = false
+        cartAmount.textContent = `${(+cartAmount.textContent + 1)
+            .toLocaleString('ru-RU')}`
+    }
+
+    if (items[id].amount == items[id].totalAmount) document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-plus')
+        .disabled = true
+
+    if (document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-plus')
+        .disabled) return
+
+    if (items[id].totalAmount - items[id].amount < 4) document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-availability')
+        .textContent = `Осталось ${items[id]
+            .totalAmount - items[id].amount - 1} шт.`
+
+    newPriceEl.textContent = `${(round(items[id].totalPrice + price))
+        .toLocaleString('ru-RU')} сом`
+    items[id].totalPrice = round(items[id].totalPrice + price)
+    items[id].amount++
+    newAmountEl.textContent = `${(items[id].amount)
+        .toLocaleString('ru-RU')}`
+    increaseSummaryPrice(price)
+    increaseAmount(id, 1)
+}
+
+function itemMinus(id, price, newPriceEl, newAmountEl) {
+    if (document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-minus')
+        .disabled) return
+
+    if (items[id].amount == 1) {
+        document
+            .querySelector('.cart[data-id="' + id + '"]')
+            .querySelector('.cart__amount-button-minus')
+            .disabled = true
+        cartAmount.textContent = `${(cartAmount.textContent - 1).toLocaleString('ru-RU')}`
+    }
+
+    if (document
+        .querySelector('.cart[data-id="' + id + '"]')
+        .querySelector('.cart__amount-button-plus').disabled)
+        document.querySelector('.cart[data-id="' + id + '"]')
+            .querySelector('.cart__amount-button-plus').disabled = false
+
+    if (items[id].totalAmount - items[id].amount < 2)
+        document
+            .querySelector('.cart[data-id="' + id + '"]')
+            .querySelector('.cart__amount-availability')
+            .textContent = `Осталось ${(items[id]
+                .totalAmount - items[id].amount + 1)
+                .toLocaleString('ru-RU')} шт.`
+
+    if (items[id].totalAmount - items[id].amount == 2)
+        document
+            .querySelector('.cart[data-id="' + id + '"]')
+            .querySelector('.cart__amount-availability')
+            .textContent = ``
+
+    newPriceEl.textContent =
+        `${(round(items[id].totalPrice - price))
+            .toLocaleString('ru-RU')} сом`
+    items[id].totalPrice = round(items[id].totalPrice - price)
+    items[id].amount--
+    newAmountEl.textContent = `${(items[id].amount)
+        .toLocaleString('ru-RU')}`
+    reduceSummaryPrice(price)
+
+    reduceAmount(id, 1)
+}
+
+function changeVisibility(element) {
+    let id = element.closest('li').dataset.id
+    if (element.checked) {
+        reduceSummaryPrice(items[id].totalPrice)
+        reduceSummaryPastPrice(items[id].pastTotalPrice)
+        cartAmount.textContent = `${cartAmount.textContent - 1}`
+        reduceAmount(id, items[id].amount)
+        buttonsDisabled(element)
+    }
+    else {
+        increaseSummaryPrice(items[id].totalPrice)
+        increaseSummaryPastPrice(items[id].pastTotalPrice)
+        cartAmount.textContent = `${+cartAmount.textContent + 1}`
+        increaseAmount(id, items[id].amount)
+        buttonsEnabled(element)
+    }
+    updateCost()
+}
+
+function deleteItem(element) {
+    let id = element.closest('li').dataset.id
+    reduceSummaryPrice(items[id]?.totalPrice)
+    reduceSummaryPastPrice(items[id]?.pastTotalPrice)
+    updateCost()
+    element.closest('li').style.display = 'none'
+    cartAmount.textContent = `${cartAmount.textContent - 1}`
+    reduceAmount(id, items[id]?.amount)
+}
+
+function deleteMissingItem(element) {
+    element.closest('li').style.display = 'none'
+}
+
+function changeCheckboxes(element) {
+    let elements = document.querySelectorAll('.cart')
+
+    if (element.checked) {
+        summaryPrice = 0
+        summaryPriceText.textContent = `${(summaryPrice)
+            .toLocaleString('ru-RU')} сом`
+        summaryPastPrice = 0
+        summaryPastPriceText.textContent = `${(summaryPastPrice)
+            .toLocaleString('ru-RU')} сом`
+        updateCost()
+        cartAmount.textContent = `0`
+
+        elements.forEach(el => {
+            reduceAmount(el.dataset.id, items[el.dataset.id].amount)
+            buttonsDisabled(el)
+        })
+    }
+    else {
+        elements.forEach((el) => {
+            if (el.querySelector('.checkbox-single').checked
+                && el.style.display !== 'none') {
+                let id = el.dataset.id
+                increaseSummaryPrice(items[id].totalPrice)
+                increaseSummaryPastPrice(items[id].pastTotalPrice)
+                updateCost()
+                cartAmount.textContent =
+                    `${+cartAmount.textContent + 1}`
+                increaseAmount(id, items[id].amount)
+                buttonsEnabled(el)
+            }
+        })
+    }
+
+    let checkboxes = document.querySelectorAll('.checkbox-single')
+    checkboxes.forEach((checkbox) => {
+        element.checked
+            ? checkbox.checked = true
+            : checkbox.checked = false
+    })
+}
+
+function addDeliveryDate(id, num) {
+    const deliveryDiv = `
+        <div class="delivery__date delivery__date-additional">
+            <p class="delivery__date-question">7-8 февраля</p>
+            <div class="delivery__date-answer">
+                <div>
+                    <img data-id="${id}" class="delivery__img" src="./images/${items[id].img}.jpg" width="40" height="56" alt="${items[id].img}">
+                    <span>${num}</span>
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector('.delivery__refusal')
+        .insertAdjacentHTML('beforebegin', deliveryDiv);
+}
+
+function reduceAmount(id, amount) {
+    if (document
+        .querySelector('.delivery__date-additional')
+        ?.querySelector('img[data-id="' + id + '"]')) {
+        let reducedSpan = document
+            .querySelector('.delivery__date-additional')
+            .querySelector('img[data-id="' + id + '"]').nextElementSibling
+        if (+reducedSpan.textContent - amount == 0) {
+            reducedSpan.closest('div').remove()
+            if (document
+                .querySelector('.delivery__date-additional')
+                .querySelector('.delivery__date-answer')) {
+                document
+                    .querySelector('.delivery__date-additional')
+                    .remove()
+            }
+        }
+        else if (+reducedSpan.textContent - amount < 0) {
+            reducedSpan.closest('div').remove()
+            if (document
+                .querySelector('.delivery__date-additional')
+                .querySelector('.delivery__date-answer')) {
+                document
+                    .querySelector('.delivery__date-additional')
+                    .remove()
+            }
+            document
+                .querySelector('.delivery__date')
+                ?.querySelector('img[data-id="' + id + '"]').closest('div').remove()
+        }
+        else {
+            reducedSpan.textContent = `${+reducedSpan.textContent - amount}`
+        }
+    }
+    else {
+        if (document
+            .querySelector('.delivery__date')
+            .querySelector('img[data-id="' + id + '"]')) {
+            let reducedSpan = document
+                .querySelector('.delivery__date')
+                .querySelector('img[data-id="' + id + '"]')
+                ?.nextElementSibling
+            if (+reducedSpan.textContent - amount == 0) {
+                reducedSpan.closest('div').remove()
+            }
+            reducedSpan.textContent = `${+reducedSpan.textContent - amount}`
+        }
+    }
+}
+
+function increaseAmount(id, amount) {
+    if (document
+        .querySelector('.delivery__date-additional')
+        ?.querySelector('img[data-id="' + id + '"]')) {
+        let reducedSpan = document
+            .querySelector('.delivery__date-additional')
+            .querySelector('img[data-id="' + id + '"]').nextElementSibling
+        reducedSpan.textContent = `${+reducedSpan.textContent + amount}`
+    }
+    else {
+        if (!document
+            .querySelector('.delivery__date')
+            ?.querySelector('img[data-id="' + id + '"]')) {
+            amount > 184
+                ? itemsPerRow = 184
+                : itemsPerRow = amount
+            deliveryRow = `
+            <div>
+                <img data-id="${id}" class="delivery__img" src="./images/${items[id].img}.jpg" width="40" height="56"
+                alt="${items[id].img}">
+                <span>${itemsPerRow}</span>
+            </div>
+            `
+            document
+                .querySelector('.delivery__date-answer')
+                .insertAdjacentHTML('beforeend', deliveryRow);
+            if (amount > 184) addDeliveryDate(id, amount - 184)
+            return
+        }
+        let reducedSpan = document
+            .querySelector('.delivery__date')
+            .querySelector('img[data-id="' + id + '"]').nextElementSibling
+        if (+reducedSpan.textContent + amount >= 185 && document
+            .querySelector('.delivery__date')
+            .classList.contains('delivery__date-additional')) {
+            deliveryRow = `
+            <div>
+                <img data-id="${id}" class="delivery__img" src="./images/${items[id].img}.jpg" width="40" height="56"
+                alt="${items[id].img}">
+                <span>${1}</span>
+            </div>
+            `
+            document.querySelector('.delivery__date-answer').insertAdjacentHTML('beforeend', deliveryRow);
+        }
+        else if (+reducedSpan.textContent + amount >= 185
+            && !document
+                .querySelector('.delivery__date')
+                .classList.contains('delivery__date-additional')) {
+            addDeliveryDate(id, amount)
+        }
+        else reducedSpan.textContent =
+            `${+reducedSpan.textContent + amount}`
+    }
+}
+
+function updateCost() {
+    summaryCost.textContent = `${(summaryPrice - summaryPastPrice)
+        .toLocaleString('ru-RU')
+        } сом`
+}
+
+function increaseSummaryPrice(num) {
+    summaryPrice += num
+    summaryPriceText.textContent = `${(round(summaryPastPrice))
+        .toLocaleString('ru-RU')} сом`
+}
+
+function reduceSummaryPrice(num) {
+    summaryPrice -= num
+    summaryPriceText.textContent = `${(round(summaryPrice))
+        .toLocaleString('ru-RU')} сом`
+}
+
+function increaseSummaryPastPrice(num) {
+    summaryPastPrice += num
+    summaryPastPriceText.textContent = `${(round(summaryPastPrice))
+        .toLocaleString('ru-RU')} сом`
+}
+
+function reduceSummaryPastPrice(num) {
+    summaryPastPrice -= num
+    summaryPastPriceText.textContent = `${(round(summaryPastPrice))
+        .toLocaleString('ru-RU')} сом`
+}
+
+function buttonsEnabled(el) {
+    el.closest('.cart')
+        .querySelector('.cart__amount-button-minus')
+        .disabled = false
+    el.closest('.cart')
+        .querySelector('.cart__amount-button-plus')
+        .disabled = false
+}
+
+function buttonsDisabled(el) {
+    el.closest('.cart')
+        .querySelector('.cart__amount-button-minus')
+        .disabled = true
+    el.closest('.cart')
+        .querySelector('.cart__amount-button-plus')
+        .disabled = true
+}
